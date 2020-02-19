1828d90e3997f0b2a7bcfdcbe9fe7c1f732dbd8a
Yury Zhauniarovich <y.zhalnerovich@gmail.com>
2020-02-19 20:53:22 +0300

    Updates script for Python Workspace Configuration to install poetry

--- 2020-02-configuring-python-workspace/py_env_config.sh
+++ 2020-02-configuring-python-workspace/py_env_config.sh
@@ -3,8 +3,10 @@
 PY3="3.8.1"
 PY2="2.7.17"
 
-PY3_TOOLS=( 'ansible' 'youtube-dl' 'scrapy')
+PY3_TOOLS=( 'ansible' 'youtube-dl' 'scrapy' )
 PY2_TOOLS=( 'rename' )
+
+USE_POETRY=1
 
 # installing pyenv using pyenv-installer if it is not installed yet
 type pyenv >/dev/null 2>&1
@@ -17,6 +19,7 @@ if [ $? -eq 1 ]; then
     echo 'eval "$(pyenv init -)"' >> ~/.bashrc
     echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
     
+    # activating for the current shell session
     export PATH="${HOME}/.pyenv/bin:$PATH"
     eval "$(pyenv init -)"
     eval "$(pyenv virtualenv-init -)"
@@ -74,8 +77,54 @@ echo "Updateing ipython startup scripts to include virtual environment packages.
 ipython profile create
 curl -L http://hbn.link/hb-ipython-startup-script > ~/.ipython/profile_default/startup/00-venv-sitepackages.py
 
-echo "Updating pip and installing virtualenv..."
-pip install --upgrade pip
-pip install virtualenv
+
+if [ $USE_POETRY -eq 1 ]; then
+    echo "Installing poetry..."
+    pyenv activate tools3
+    curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
+
+    source $HOME/.poetry/env
+
+    # configuring poetry to create venv directories inside the project
+    poetry config virtualenvs.in-project true
+
+    # adding lookup of bash completion files in user's directory
+    mkdir -p ~/.bash_completion.d/
+    if ! [ -f ~/.bash_completion ]; then
+        echo '' >> ~/.bash_completion
+        echo 'for bcfile in ~/.bash_completion.d/* ; do' >> ~/.bash_completion
+        echo '    [ -f "$bcfile" ] && . $bcfile' >> ~/.bash_completion
+        echo 'done' >> ~/.bash_completion
+        
+        echo '' >> ~/.bashrc
+        echo 'if [ -f ~/.bash_completion ]; then' >> ~/.bashrc
+        echo '    source ~/.bash_completion' >> ~/.bashrc
+        echo 'fi' >> ~/.bashrc
+    fi
+
+    if ! [ -f ~/.bash_completion.d/poetry.bash-completion ]; then
+        poetry completions bash > ~/.bash_completion.d/poetry.bash-completion
+    fi
+
+    pyenv deactivate
+else
+    echo "Installing virtualenv..."
+    pyenv activate tools3
+    pip install virtualenv
+    pyenv deactivate
+
+    mkdir -p ~/.bash_fns
+    
+    curl -L https://raw.githubusercontent.com/zyrikby/blog_related/master/2020-02-configuring-python-workspace/pip_functions.sh > ~/.bash_fns/pip_functions.sh
+    
+    echo '' >> ~/.bashrc
+    echo 'if [ -f ~/.bash_fns/pip_functions.sh ]; then' >> ~/.bashrc
+    echo '    source ~/.bash_fns/pip_functions.sh' >> ~/.bashrc
+    echo 'fi' >> ~/.bashrc
+
+    #activating for current shell session
+    source ~/.bash_fns/pip_functions.sh
+fi
 
 echo "Done..."
+exec $SHELL
